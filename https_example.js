e283625605908d8874d29d3ae0d8c4b85ee04468
Andrew Kim <your@email.com>
2017-01-15 18:42:44 -0500

    Added response to callback function as an argument.

--- https_example.js
+++ https_example.js
@@ -1,3 +1,20 @@
 const https = require('https');
 
-console.log('I did it!');
+// console.log('I did it!');
+
+const options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+const callback = (response) => {
+  console.log('In response handler callback!');
+  console.log("Response:", response);
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
